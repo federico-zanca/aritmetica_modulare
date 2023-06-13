d03c718aac0747d0e3df48b7b4d402f61a0be7f6
federico-zanca <federico.zanca@mail.polimi.it>
2023-06-13 13:18:11 +0200

    Finds prime factors of a given number

---
+++ factor.py
@@ -0,0 +1,14 @@
+def factorize(n):
+    factors = []
+    d = 2
+    while d * d <= n:
+        while n % d == 0:
+            factors.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+        factors.append(n)
+    return factors
+
+n = int(input("Enter n: "))
+print(factorize(n))
