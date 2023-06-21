a49acbb689ebc3460a1244e860860a0be2019fd0
federico-zanca <federico.zanca@mail.polimi.it>
2023-06-21 18:41:16 +0200

    fixed and added support for multiple solutions

--- babygiant.py
+++ babygiant.py
@@ -1,4 +1,27 @@
 import math
+def prime_factors(n):
+    factors = []
+    d = 2
+    while d * d <= n:
+        while n % d == 0:
+            factors.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+        factors.append(n)
+    return factors
+
+def is_primitive_root(a, p):
+    if a == 1:
+        return False
+    result = pow(a, p - 1, p)
+    if result != 1:
+        return False
+    for q in prime_factors(p - 1):
+        result = pow(a, (p - 1) // q, p)
+        if result == 1:
+            return False
+    return True
 def invert(a, b):
     x, y, u, v = 0, 1, 1, 0
     while a != 0:
@@ -24,12 +47,14 @@ def pow(a, b, n):
     return x
 
 def babystep_giantstep(alfa, beta, p):
+    flag = is_primitive_root(alfa, p)
     n = int(math.ceil(math.sqrt(p - 1)))
     print(str(alfa) + "**x (mod " + str(p) + ") == " + str(beta))
     print("Riscrivo x in base N \nN=ceil(sqrt("+str(p-1)+"))="+str(n))
     print("jk\t a**j\t b*a**(-N*k)")
     j_values = []
     k_values = []
+    scelti = []
     for j in range(n):
         alfa_j = pow(alfa, j, p)
         j_values.append(alfa_j)
@@ -37,16 +62,28 @@ def babystep_giantstep(alfa, beta, p):
         k_values.append(beta_alfa_nk)
         print(str(j)+"\t "+str(alfa_j)+"\t "+str(beta_alfa_nk))
         if(beta_alfa_nk in j_values):
-            print("x = j"+" + "+str(n)+"k")
-            print("j="+str(j_values.index(beta_alfa_nk))+", k="+str(k_values.index(beta_alfa_nk)))
-            return j + n * k_values.index(beta_alfa_nk)
+            #scelti.append([j, k_values.index(beta_alfa_nk)])
+            scelti.append([j_values.index(beta_alfa_nk), j])
+            if(flag):
+                break
+            #print("x = j"+" + "+str(n)+"k")
+            #print("j="+str(j_values.index(beta_alfa_nk))+", k="+str(k_values.index(beta_alfa_nk)))
+            #return j + n * k_values.index(beta_alfa_nk)
         elif(alfa_j in k_values):
-            print("x = j"+" + "+str(n)+"k")
-            print("j="+str(j)+", k="+str(k_values.index(alfa_j)))
-            return j + n * k_values.index(alfa_j)
-
+            scelti.append([j, k_values.index(alfa_j)])
+            if(flag):
+                break
+            #print("x = j"+" + "+str(n)+"k")
+            #print("j="+str(j)+", k="+str(k_values.index(alfa_j)))
+            #return j + n * k_values.index(alfa_j)
+    print("x = j"+" + "+str(n)+"k")
+    for i in range(len(scelti)):
+        j= scelti[i][0]
+        k= scelti[i][1]
+        print("j="+str(j)+", k="+str(k))
+        print("x"+str(i)+"="+str(j+n*k))
 print("Enter alfa**x == beta (mod p)")
 alfa = int(input("Enter alfa: "))
 beta = int(input("Enter beta: "))
 p = int(input("Enter p: "))
-print("x="+str(babystep_giantstep(alfa, beta, p)))
+(babystep_giantstep(alfa, beta, p))
